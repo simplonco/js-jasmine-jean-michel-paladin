e71cae74300cb36c66e2d4c3af9ff0f7e823b856
Sandro Micalizzi <hello@sandromicalizzi.com>
2017-06-21 23:35:21 +0200

    Squelette et consignes

---
+++ app/class/Character.js
@@ -0,0 +1,28 @@
+module.exports = class Character {
+
+  constructor(name) {
+    // Your code
+
+  }
+
+  toString() {
+    // Your code
+
+  }
+
+  _calculateDamage(min, max, bonus) {
+    // Your code
+
+  }
+
+  _suffersAttack(damagePoints) {
+    // Your code
+
+  }
+
+  attacks(character) {
+    // Your code
+    
+  }
+
+}
